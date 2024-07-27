a3648abb89bceae46f6933c507caf80218707837
Shinovon <arman200711@gmail.com>
2024-07-27 23:14:26 +0500

    config, static building

--- include/mbedtls/mbedtls_config.h
+++ include/mbedtls/mbedtls_config.h
@@ -1111,7 +1111,7 @@
  * This option is only useful if both MBEDTLS_SHA256_C and
  * MBEDTLS_SHA512_C are defined. Otherwise the available hash module is used.
  */
-//#define MBEDTLS_ENTROPY_FORCE_SHA256
+#define MBEDTLS_ENTROPY_FORCE_SHA256
 
 /**
  * \def MBEDTLS_ENTROPY_NV_SEED
@@ -2655,7 +2655,7 @@
  *
  * Uncomment to enable the LMS verification algorithm and public key operations.
  */
-//#define MBEDTLS_LMS_C
+#define MBEDTLS_LMS_C
 
 /**
  * \def MBEDTLS_LMS_PRIVATE
