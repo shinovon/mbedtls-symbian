6438874412718748c4062dbd529613bb6fe584ef
Shinovon <arman200711@gmail.com>
2024-07-30 16:00:55 +0500

    disable debug
    cuz it's slow

--- library/debug.c
+++ library/debug.c
@@ -386,4 +386,6 @@ void mbedtls_debug_printf_ecdh(const mbedtls_ssl_context *ssl, int level,
 }
 #endif /* MBEDTLS_ECDH_C */
 
-#endif /* MBEDTLS_DEBUG_C */
+#else /* MBEDTLS_DEBUG_C */
+EXPORT_C void mbedtls_debug_set_threshold(int threshold) {}
+#endif
